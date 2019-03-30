d2e7baac75540607218aa81b0ab0aa5767c68175
Siyu Xiang <siyuxiang@SiyudeMacBook-Air.local>
2019-03-30 15:11:33 -0700

    Achieve the intent of EnterRating

--- app/src/main/java/mealrater/siyuxiang/com/mealrater/RatingActivity.java
+++ app/src/main/java/mealrater/siyuxiang/com/mealrater/RatingActivity.java
@@ -1,7 +1,10 @@
 package mealrater.siyuxiang.com.mealrater;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RatingActivity extends AppCompatActivity {
 
@@ -9,6 +12,21 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_rating);
+        initEnterRating();
+    }
+
+    //Siyu
+    //initialize Enter Rating Button
+    private void initEnterRating() {
+        Button buttonEnterRating = (Button) findViewById(R.id.buttonEnterRating);
+        buttonEnterRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RatingActivity.this, RestaurantListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 
 }
